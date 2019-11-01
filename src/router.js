ff0a8a2c36a9100bcccb39d6c9e450f607174201
Fidel Guajardo <fidel.guajardo@harlandclarke.com>
2019-10-31 22:41:12 -0500

    fix: remove App from root route

--- src/router.js
+++ src/router.js
@@ -1,23 +1,16 @@
 import Router from "vue-router";
-import App from "@/App";
 import Search from "@/components/Search";
 import Favorites from "@/components/Favorites";
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      component: App,
-      children: [
-        {
-          path: "",
-          component: Search
-        },
-        {
-          path: "/favorites",
-          component: Favorites
-        }
-      ]
+      path: "",
+      component: Search
+    },
+    {
+      path: "/favorites",
+      component: Favorites
     }
   ]
 });
